ffcfd04f740826cefb5a3ab633370d1bb88df1d5
ElRoccinante <alaechouiye@gmail.com>
2023-11-25 13:12:05 -0800

    h

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,6 @@
-# install a package flask
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
